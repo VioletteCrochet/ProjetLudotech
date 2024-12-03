42d14e532d5fcbbb63599a968d70f3c7eb969990
VioletteCrochet <violettecrc@gmail.com>
2024-12-03 13:52:18 +0100

    refactor (database): update database script

--- src/main/resources/sql/ludodb.sql
+++ src/main/resources/sql/ludodb.sql
@@ -2,20 +2,20 @@ DROP TABLE IF EXISTS Clients, Jeux, Genres, Jeux_Genres;
 
 CREATE TABLE Clients (
     id serial Constraint PK_Clents primary key,
-    nom VARCHAR(50), -- Taille entre 3 et 50
-    prenom VARCHAR(50), -- Taille entre 3 et 50
-    email VARCHAR(50), -- Taille entre 3 et 50, validation email côté applicatif
-    numTel VARCHAR(50), -- Optionnel, taille entre 3 et 50
-    rue VARCHAR(50), -- Taille entre 3 et 50
-    cpo VARCHAR(50), -- Taille entre 3 et 50
-    ville VARCHAR(50) 
+    nom VARCHAR(50)NOT NULL, 
+    prenom VARCHAR(50)NOT NULL, 
+    email VARCHAR(50) CONSTRAINT UK_email_unique UNIQUE, 
+    numTel VARCHAR(50)NULL,
+    rue VARCHAR(50)NOT NULL, 
+    cpo VARCHAR(50)NOT NULL, 
+    ville VARCHAR(50)NOT NULL 
 	);
 	
 CREATE TABLE Jeux (
     id serial Constraint PK_Jeux PRIMARY KEY,
     titre VARCHAR(255) NOT NULL,
     reference VARCHAR(255) NOT NULL,
-    description TEXT,
+    description TEXT NULL,
     tarifJour DECIMAL(10, 2) NOT NULL,
     ageMin INT NOT NULL,
     duree INT
@@ -25,12 +25,12 @@ CREATE TABLE Genres (
     libelle VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Jeux_Genres (
+CREATE TABLE Jeu_Genre (
     jeu_id INT,
     genre_id INT,
     PRIMARY KEY (jeu_id, genre_id),
-    FOREIGN KEY (jeu_id) REFERENCES Jeu(id),
-    FOREIGN KEY (genre_id) REFERENCES Genre(id)
+    FOREIGN KEY (jeu_id) REFERENCES Jeux(id),
+    FOREIGN KEY (genre_id) REFERENCES Genres(id)
 );
 
 INSERT INTO Clients (nom, prenom, email, numTel, rue, cpo, ville) 
@@ -41,4 +41,16 @@ VALUES
     ('Dubois', 'Lucie', 'lucie.dubois@example.com', '0414556677', 'place de la République', '69001', 'Lyon'),
     ('Petit', 'Alain', 'alain.petit@example.com', '0516778899', 'chemin des Écoliers', '33000', 'Bordeaux');
 
+INSERT INTO Jeux (titre, reference, description, tarifJour, ageMin, duree)
+VALUES 
+    ('Monopoly', 'MON123', 'Jeu de société classique', 10.00, 8, 120),
+    ('Catan', 'CAT456', 'Jeu de stratégie sur une île', 12.50, 12, 90);
+
+
+INSERT INTO Genres (libelle) 
+VALUES 
+    ('Stratégie'),
+    ('Aventure'),
+    ('Famille');
+	
 select * from Clients;
